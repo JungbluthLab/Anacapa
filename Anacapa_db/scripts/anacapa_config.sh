807c995d9a010c98e10c05f968ac47b548c36097
Sean Jungbluth <jungbluth.sean@gmail.com>
2021-03-16 19:38:34 -0700

    Checkpoint

--- Anacapa_db/scripts/anacapa_config.sh
+++ Anacapa_db/scripts/anacapa_config.sh
@@ -17,7 +17,7 @@ MODULE_SOURCE="" 	#if none, leave empty <- for HPC
 ### if not loading modules, need a way to not run lines ~65 - 70 (module load bit...) and a way to run programs from load names not paths...
 
 #defines what program should launch the run scripts inside the submitted jobs. can change to e.g. singularity if running in a container
-RUNNER="bash"
+#RUNNER="bash"
 
 #load cutadapt
 CUTADAPT="/usr/local/anacapa/miniconda/bin/cutadapt" 		#version 1.16 path to cutadapt binary. See documentation for how to obtain this script
